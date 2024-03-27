0ba9e7948bb15a4718b98fadad666b11a52f53d0
Joni <jonneldator2013@gmail.com>
2024-03-28 00:16:35 +0800

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToFacultyPage() {
+    window.location.href = 'faculty.html'; // Replace 'faculty.html' with the actual URL of your faculty page
+}
+
+function goToStudentsPage() {
+    window.location.href = 'homepage.html'; // Replace 'students.html' with the actual URL of your students page
+}
